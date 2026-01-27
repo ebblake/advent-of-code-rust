13ece7d6a535a807cbc0dda623bb1a7b006ac5b9
maneatingape <44142177+maneatingape@users.noreply.github.com>
2026-01-27 23:55:35 +0000

    Use different dynamnic programming approach for part one and two

--- src/year2015/day17.rs
+++ src/year2015/day17.rs
@@ -5,43 +5,48 @@
 //! This will work but is a little slow as there are 20 containers, giving 2²⁰ = 1048576
 //! combinations to check.
 //!
-//! Tackle this with dynamic programming.  All we really have to compute is how
-//! many ways a given group size can be reached for any given item index used as
-//! the next item added to a smaller group whose item(s) are all smaller indices.
-//! Part 1 is then the sum of all counted group sizes that summed to 150, while
-//! Part 2 is the counter for the smallest size.
+//! Tackling this with dynamic programming provides a much faster approach.
 use crate::util::parse::*;
 
 const GOAL: usize = 150;
-const ITEMS: usize = 20;
 
-pub fn parse(input: &str) -> [u32; ITEMS + 1] {
-    // Initialize an array to collect counts of reachable group sizes.
-    let mut counts = [[0; ITEMS + 1]; GOAL + 1];
-    let mut cap = 0;
-    counts[0][0] = 1;
+pub fn parse(input: &str) -> Vec<usize> {
+    input.iter_unsigned().collect()
+}
 
-    // For each container, update ways to reach group sizes, starting from largest sum.
-    for (rank, size) in input.iter_unsigned().enumerate() {
-        cap += size;
-        let goal = if cap < GOAL { cap } else { GOAL };
-        for i in (size..goal + 1).rev() {
-            for j in 0..rank + 1 {
-                counts[i][j + 1] += counts[i - size][j];
+pub fn part1(input: &[usize]) -> u32 {
+    let mut ways = [0; GOAL + 1];
+    ways[0] = 1;
+
+    for &item in input {
+        for i in (item..GOAL + 1).rev() {
+            ways[i] += ways[i - item];
+        }
+    }
+
+    ways[GOAL]
+}
+
+pub fn part2(input: &[usize]) -> u32 {
+    let mut ways = [0; GOAL + 1];
+    ways[0] = 1;
+
+    let mut minimum = [u32::MAX; GOAL + 1];
+    minimum[0] = 0;
+
+    for &item in input {
+        for i in (item..GOAL + 1).rev() {
+            let take = minimum[i - item].saturating_add(1);
+            let not_take = minimum[i];
+
+            if take < not_take {
+                ways[i] = ways[i - item];
+                minimum[i] = take;
+            } else if take == not_take {
+                ways[i] += ways[i - item];
             }
         }
     }
 
-    // Result is row 150.
-    counts[GOAL]
-}
-
-/// We only care about the total combinations, so sum the entire vec.
-pub fn part1(input: &[u32]) -> u32 {
-    input.iter().sum()
-}
-
-/// We want the number of combinations with the fewest containers, so find first non-zero value.
-pub fn part2(input: &[u32]) -> u32 {
-    *input.iter().find(|&&n| n > 0).unwrap()
+    ways[GOAL]
 }
